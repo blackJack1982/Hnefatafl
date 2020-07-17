9ab676af6b36fab9fe99f19f409fdbd7f8eb2860
Stéphane <Stephane.Devaux@gmail.com>
2020-07-17 12:28:11 +0200

    Use a dict/hash table for proper adressing

--- engine.steph.pl
+++ engine.steph.pl
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
 from pprint import pprint #to display the array
 
+#1. Create an empty board
+
 N = 11 # could be 9 for 9X9 or 11 for 11X11
+board = { (i,j):'-' for i in range(11) for j in range(11) } # create NXN board filled with '-'
 
-board = [ ['-'] * N ] * N # create NXN board filled with '-'
-
+#USEFUL: 
 def status(board):
 	"""
 	Gives the status of the board ==> Display it
 	"""
-	pprint(board)
+	for i in range(N):
+		for j in range (N):
+			print (f" '{board[i,j]}' ", end='')
+		print ("")
 
+#2. Positioning of the pieces
+
+if N == 11:
+#board[0][3:8]    = 5 * ['B']
+#	board[3:8][0]    = 5 * ['B']
+#	board[N-1][3:8]  = 5 * ['B']
+	#board[3:8][N-1]  = 5 * ['B']
+	pass	
+elif N == 9:
+	print ("Not implemented yet")
+	exit()
+
+
+#TESTS:
 status(board)
