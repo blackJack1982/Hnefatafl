bba9e697958778c91bc15bcb1cf98bfafd0b113e
Stéphane <Stephane.Devaux@gmail.com>
2020-07-17 11:35:52 +0200

    creation of an empty board

--- engine.steph.pl
+++ engine.steph.pl
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+from pprint import pprint #to display the array
 
+N = 11 # could be 9 for 9X9 or 11 for 11X11
+
+board = [ ['-'] * N ] * N # create NXN board filled with '-'
+
+def status(board):
+	"""
+	Gives the status of the board ==> Display it
+	"""
+	pprint(board)
+
+status(board)
