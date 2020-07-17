ada8cbf9c62246e84aea79f6e4027b91c15f690e
Stéphane <Stephane.Devaux@gmail.com>
2020-07-17 14:29:32 +0200

    first layout of pieces on the board

--- engine.steph.pl
+++ engine.steph.pl
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from pprint import pprint #to display the array
 
 #1. Create an empty board
 
@@ -8,25 +7,35 @@ board = { (i,j):'-' for i in range(11) for j in range(11) } # create NXN board f
 
 #USEFUL: 
 def status(board):
-	"""
+    """
 	Gives the status of the board ==> Display it
 	"""
-	for i in range(N):
-		for j in range (N):
-			print (f" '{board[i,j]}' ", end='')
-		print ("")
+    for i in range(N):
+        for j in range (N):
+            print (f" '{board[i,j]}' ", end='')
+        print ("")
 
 #2. Positioning of the pieces
 
 if N == 11:
-#board[0][3:8]    = 5 * ['B']
-#	board[3:8][0]    = 5 * ['B']
-#	board[N-1][3:8]  = 5 * ['B']
-	#board[3:8][N-1]  = 5 * ['B']
-	pass	
+    for j in range (3,8):
+        board[  0, j] = 'B'
+        board[N-1, j] = 'B'
+        board[  5, j] = 'W'
+        board[  4, j] = 'W' if j in range(4,7) else '-'
+        board[  6, j] = 'W' if j in range(4,7) else '-'
+    for i in range (3,8):
+        board[ i,   0] = 'B'
+        board[ i, N-1] = 'B'	
+        board[ i,   5] = 'W'
+    board[   1,   5 ] = 'B'
+    board[   5,   1 ] = 'B'
+    board[ N-2,   5 ] = 'B'
+    board[   5, N-2 ] = 'B'
+    #       board[ i,   4] = 'W' if j in range(4,7) else '-'
 elif N == 9:
-	print ("Not implemented yet")
-	exit()
+    print ("Not implemented yet")
+    exit()
 
 
 #TESTS:
