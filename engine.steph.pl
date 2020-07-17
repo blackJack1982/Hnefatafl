2851e1f846a32c3463227943f06256e7e8f68486
Stéphane <Stephane.Devaux@gmail.com>
2020-07-17 19:02:08 +0200

    now status() returns a string needed for the gui still wondering how to integrate to allow the gui to display stuff by itself

--- engine.steph.pl
+++ engine.steph.pl
@@ -18,22 +18,22 @@ class Board():
         # create NXN board filled with '-'
         self.board : dict = {(i,j):'-' for i in range(self.N) for j in range(self.N)}      
         # Pre-positionning of the player's pieces
-        if N == 11:
-            for j in range (3,8):
-                self.board[  0, j] = 'B'
-                self.board[N-1, j] = 'B'
-                self.board[  5, j] = 'W'
-                self.board[  4, j] = 'W' if j in range(4,7) else '-'
-                self.board[  6, j] = 'W' if j in range(4,7) else '-'
+        if self.N == 11:
+            for j in range (      3, 8):
+                self.board[       0, j] = 'B'
+                self.board[self.N-1, j] = 'B'
+                self.board[       5, j] = 'W'
+                self.board[       4, j] = 'W' if j in range(4,7) else '-'
+                self.board[       6, j] = 'W' if j in range(4,7) else '-'
             for i in range (3,8):
-                self.board[ i,   0] = 'B'
-                self.board[ i, N-1] = 'B'	
-                self.board[ i,   5] = 'W'
-            self.board[   1,   5 ] = 'B'
-            self.board[   5,   1 ] = 'B'
-            self.board[ N-2,   5 ] = 'B'
-            self.board[   5, N-2 ] = 'B'
-            self.board[   5,   5 ] = 'K'
+                self.board[ i,        0] = 'B'
+                self.board[ i, self.N-1] = 'B'	
+                self.board[ i,        5] = 'W'
+            self.board[        1,        5] = 'B'
+            self.board[        5,        1] = 'B'
+            self.board[ self.N-2,        5] = 'B'
+            self.board[        5, self.N-2] = 'B'
+            self.board[        5,        5] = 'K'
         elif N == 9:
             print ("Not implemented yet")
             exit()
@@ -63,16 +63,19 @@ class Board():
         """
         Gives the status of the board ==> Display it
         """
-        for i in range(N):
-            for j in range (N):
-                print (f" '{self.board[i,j]}' ", end='')
-            print ("")
+        output =''
+        for i in range(self.N):
+            for j in range (self.N):
+                output += f" '{self.board[i,j]}' "
+            output += '\n'
+        
+        return output
 
 
 if __name__ == '__main__':
     N=11 #Size of the board NXN
     board = Board(N)
-    board.status()
+    print (board.status())
     """
     '-'  '-'  '-'  'B'  'B'  'B'  'B'  'B'  '-'  '-'  '-'
     '-'  '-'  '-'  '-'  '-'  'B'  '-'  '-'  '-'  '-'  '-'
